80aae41abb4eae1b098995a03193d13c0c5d08f9
vivekdhir77 <vivekdhir77@gmail.com>
2025-10-19 16:41:00 -0400

    Yolo bounding boxes and choosing objects

--- VideoStream.py
+++ VideoStream.py
@@ -1,30 +1,64 @@
+from ultralytics import YOLO
 import cv2
+import math 
+# start webcam
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
 
-# Open the default camera
-cam = cv2.VideoCapture(1)
+# model
+model = YOLO("yolo-Weights/yolov8n.pt")
 
-# Get the default frame width and height
-frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# object classes
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
 
-# Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (frame_width, frame_height))
 
 while True:
-    ret, frame = cam.read()
+    success, img = cap.read()
+    
+    height, width = img.shape[:2]
+    center_x, center_y = width // 2, height // 2
+    cv2.circle(img, (center_x, center_y), 5, (0, 0, 255), -1)
+    
+    results = model(img, stream=True)
 
-    # Write the frame to the output file
-    out.write(frame)
+    for r in results:
+        boxes = r.boxes
 
-    # Display the captured frame
-    cv2.imshow('Camera', frame)
+        for box in boxes:
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
 
-    # Press 'q' to exit the loop
+            if (x1 <= center_x <= x2) and (y1 <= center_y <= y2):
+                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+                confidence = math.ceil((box.conf[0]*100))/100
+                print("Confidence --->",confidence)
+
+                cls = int(box.cls[0])
+                print("Class name -->", classNames[cls])
+
+                org = [x1, y1]
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                fontScale = 1
+                color = (255, 0, 0)
+                thickness = 2
+
+                cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+
+    cv2.imshow('Camera', img)
     if cv2.waitKey(1) == ord('q'):
         break
 
-# Release the capture and writer objects
-cam.release()
-out.release()
+cap.release()
 cv2.destroyAllWindows()
